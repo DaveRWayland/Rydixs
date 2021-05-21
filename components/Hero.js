cc01894074e83dc61112b57da29ec6b3d01132ef
David Raygoza <draygoza@ecomsur.com>
2021-05-21 01:09:24 -0500

    change names

--- components/Hero.js
+++ components/Hero.js
@@ -13,7 +13,7 @@ const Hero = () => {
               alt="First slide"
             />
             <Carousel.Caption>
-              <h3>First slide label</h3>
+              <h3>Renata</h3>
               <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
             </Carousel.Caption>
           </Carousel.Item>
@@ -25,7 +25,7 @@ const Hero = () => {
             />
 
             <Carousel.Caption>
-              <h3>Second slide label</h3>
+              <h3>Ricardo</h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </Carousel.Caption>
           </Carousel.Item>
